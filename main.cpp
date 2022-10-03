d9c32810e88dba920301a8f1ce38b5ced3096044
lk321 <test@test.org>
2022-10-03 17:50:31 +0300

    add files

--- main.cpp
+++ main.cpp
@@ -1,27 +1,5 @@
 #include <bits/stdc++.h>
 
-struct VectorInt {    
-    int* data;
-    int size;
-
-    void initialize(int new_sz) {
-        size = new_sz;
-        data = new int[size];
-    }
-
-    void erase() {
-        delete[] data;
-    }
-
-    int& get(const int& idx) {
-        // bruh no validity check
-        return data[idx];
-    }
-    const int& get(const int& idx) const {
-        return data[idx];
-    }
-};
-
 template <typename T> // T must be an integer type (signed/unsigned, pos/neg - doesn't matter).
 void dec2bin(const T& n) 
 // Print binary representation of n (output is sent to stdout) using bit masks.
@@ -33,36 +11,15 @@ void dec2bin(const T& n)
     }   
 }
 
-int main() {
-    VectorInt v{};
+template <typename T>
+void printv(const std::vector<T>& v) {
+    for (auto x : v)
+        std::cout << x << ' ';
+}
 
-    union A {
-        float value;
-        unsigned int bits;
-    };
+int main()
+{
+    std::freopen("input.txt", "r", stdin);
 
-    // 1. float -> binary
-    // { // That works! dec2bin(137.375f) won't.
-    //
-    //     // 01000011000010010110000000000000
-    //     A a { 137.375f };
-    //     dec2bin(a.bits);
-    // }
-
-    // 2. Mantissa overflow. Idk what I see when this runs.
-    // {
-    //     std::cout << std::fixed; 
-    //     std::cout.precision(2);
-    //
-    //     for (unsigned int i { 10 }; (unsigned long)i * 10 < std::numeric_limits<unsigned int>::max(); i *= 10) {
-    //         std::cout << i << "    " << std::bitset<sizeof(unsigned long) * 8>(i) << "    "; 
-    //         dec2bin( A{ (float)i }.bits ); 
-    //         std::cout << '\n';
-    //     }
-    // }
     
-    
-
-    std::cout << "\n\n";
-    return 0;
 }
