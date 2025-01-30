8ca62220c457c51563cae3aa9e39e15d8ff6e501
Alex Shewhart <storage.t@yandex.com>
2025-01-30 17:21:12 +0300

    fix

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "How are you?\n";
-  std::cout << "Hello world\n" << std::endl;
-  std::cout << "Bye! How are you?";
-  std::cout << "what";
-  
-}
